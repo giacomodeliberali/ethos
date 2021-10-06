8331a84d8d753a043134cb57e9d722922f138a06
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2021-10-06 16:50:53 +0200

    Add migrations

---
+++ server/src/Ethos.EntityFrameworkCore/Migrations/20211006130455_FixDurationAsInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ethos.EntityFrameworkCore.Migrations
+{
+    public partial class FixDurationAsInteger : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Schedules");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DurationInMinutes",
+                table: "Schedules",
+                type: "int",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DurationInMinutes",
+                table: "Schedules");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "Schedules",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+    }
+}

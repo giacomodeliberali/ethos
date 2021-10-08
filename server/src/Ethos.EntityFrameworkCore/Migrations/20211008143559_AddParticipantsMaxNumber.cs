7bbedc6a7fb3bd7342efddae68977a4ee5c747de
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2021-10-08 16:47:29 +0200

    Add migrations for ParticipantsMaxNumber

---
+++ server/src/Ethos.EntityFrameworkCore/Migrations/20211008143559_AddParticipantsMaxNumber.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ethos.EntityFrameworkCore.Migrations
+{
+    public partial class AddParticipantsMaxNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ParticipantsMaxNumber",
+                table: "Schedules",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParticipantsMaxNumber",
+                table: "Schedules");
+        }
+    }
+}

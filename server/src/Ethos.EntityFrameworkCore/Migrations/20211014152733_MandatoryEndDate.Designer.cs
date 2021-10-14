307785835799b821c0b66aaa00e7f380c4ef5f07
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2021-10-14 17:30:27 +0200

    Add migrations for mandatory end date

--- server/src/Ethos.EntityFrameworkCore/Migrations/20211014152733_MandatoryEndDate.Designer.cs
+++ server/src/Ethos.EntityFrameworkCore/Migrations/20211014152733_MandatoryEndDate.Designer.cs
@@ -4,14 +4,16 @@ using Ethos.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ethos.EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211014152733_MandatoryEndDate")]
+    partial class MandatoryEndDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/src/Ethos.EntityFrameworkCore/Migrations/20211014152733_MandatoryEndDate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ethos.EntityFrameworkCore.Migrations
+{
+    public partial class MandatoryEndDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                schema: "Schedules",
+                table: "Recurring",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                schema: "Schedules",
+                table: "Recurring",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}

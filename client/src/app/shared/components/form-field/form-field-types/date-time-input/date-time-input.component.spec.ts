9efbe72ab5feb6388afaaa5c6a168cd92af80aee
Federico Galfione <federico.galfione@outlook.com>
2022-01-11 19:30:47 +0100

    Fix test fiels linting

--- client/src/app/shared/components/form-field/form-field-types/date-time-input/date-time-input.component.spec.ts
+++ client/src/app/shared/components/form-field/form-field-types/date-time-input/date-time-input.component.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { DateTimeInputComponent } from './date-time-input.component';
 
 describe('DateTimeInputComponent', () => {
   let component: DateTimeInputComponent;
   let fixture: ComponentFixture<DateTimeInputComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DateTimeInputComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [DateTimeInputComponent],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(DateTimeInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(DateTimeInputComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- client/src/app/shared/components/form-field/form-field-types/password-input/password-input.component.spec.ts
+++ client/src/app/shared/components/form-field/form-field-types/password-input/password-input.component.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { PasswordInputComponent } from './password-input.component';
 
 describe('PasswordInputComponent', () => {
   let component: PasswordInputComponent;
   let fixture: ComponentFixture<PasswordInputComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PasswordInputComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [PasswordInputComponent],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(PasswordInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(PasswordInputComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();

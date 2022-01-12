ef4400f86fe8b24cc27a86f245ddd8bb29dd86a1
Federico Galfione <federico.galfione@outlook.com>
2022-01-12 17:40:54 +0100

    Add custom validators to days and endDate formcontrols

--- client/src/app/admin/components/create-edit-schedule-modal/create-edit-schedule-modal.component.ts
+++ client/src/app/admin/components/create-edit-schedule-modal/create-edit-schedule-modal.component.ts
@@ -1,12 +1,30 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserDto } from '@core/services/ethos.generated.service';
 import { ModalController } from '@ionic/angular';
+
+const daysValidator = (control: FormControl) => {
+  const isRecurrent = control.parent?.get('isRecurrent').value;
+  return isRecurrent && control.value.length <= 0 ? { required: false } : null;
+};
+
+const endDateValidator = (control: FormControl) => {
+  const isRecurrent = control.parent?.get('isRecurrent').value;
+  return isRecurrent ? Validators.required(control) : null;
+};
 
 @Component({
   selector: 'app-create-edit-schedule-modal',
   templateUrl: './create-edit-schedule-modal.component.html',
   styleUrls: ['./create-edit-schedule-modal.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateEditScheduleModalComponent implements OnInit {
   @Input()
@@ -38,15 +56,16 @@ export class CreateEditScheduleModalComponent implements OnInit {
     description: new FormControl(null, [Validators.required]),
     isRecurrent: new FormControl(false),
     startDate: new FormControl(null, [Validators.required]),
-    endDate: new FormControl(null),
+    endDate: new FormControl(null, [endDateValidator]),
     time: new FormControl(null, [Validators.required]),
-    days: new FormControl([], [Validators.required]),
+    days: new FormControl([], [daysValidator]),
     organizerId: new FormControl(null, [Validators.required]),
     participantsMaxNumber: new FormControl(null, [Validators.required]),
   });
 
   set isRecurrent(val: boolean) {
     this.scheduleForm.get('isRecurrent').setValue(val);
+    this.scheduleForm.get('days').updateValueAndValidity();
   }
 
   get isRecurrent(): boolean {

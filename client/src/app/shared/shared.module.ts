8a063c0f4f55e207840745aeae705d9c848ea626
Federico Galfione <federico.galfione@outlook.com>
2022-01-13 17:50:50 +0100

    Update SharedModule

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { DateTimeInputComponent } from './components/form-field/form-field-types/date-time-input/date-time-input.component';
 import { DefaultInputComponent } from './components/form-field/form-field-types/default-input/default-input.component';
+import { InfoInputComponent } from './components/form-field/form-field-types/info-input/info-input.component';
 import { PasswordInputComponent } from './components/form-field/form-field-types/password-input/password-input.component';
 import { TextareaInputComponent } from './components/form-field/form-field-types/textarea-input/textarea-input.component';
 import { FormFieldComponent } from './components/form-field/form-field.component';
@@ -12,8 +13,8 @@ import { LoadingComponent } from './components/loading-toast/loading.component';
 import { LogoutModalComponent } from './components/logout-modal/logout-modal.component';
 import { ModalDefaultContentComponent } from './components/modal-default-content/modal-default-content.component';
 import { ScheduleCardComponent } from './components/schedule-card/schedule-card.component';
-import { UserSelectorComponent } from './components/user-selector/user-selector.component';
 import { UnsavedChangesComponent } from './components/unsaved-changes/unsaved-changes.component';
+import { UserSelectorComponent } from './components/user-selector/user-selector.component';
 import { WeekDaySelectorComponent } from './components/week-day-selector/week-day-selector.component';
 
 @NgModule({
@@ -31,6 +32,7 @@ import { WeekDaySelectorComponent } from './components/week-day-selector/week-da
     PasswordInputComponent,
     TextareaInputComponent,
     DateTimeInputComponent,
+    InfoInputComponent,
   ],
   imports: [CommonModule, IonicModule, ReactiveFormsModule],
   exports: [

b33835d4305f2c6f5f2ca6e461cee118f1a0a9ab
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2022-02-22 20:14:11 +0100

    Fix NextOccurrences calculation

--- server/src/Ethos.Application/Handlers/GetAllRecurringSchedulesQueryHandler.cs
+++ server/src/Ethos.Application/Handlers/GetAllRecurringSchedulesQueryHandler.cs
@@ -6,50 +6,91 @@ using System.Threading.Tasks;
 using Cronos;
 using Ethos.Application.Commands;
 using Ethos.Application.Contracts.Schedule;
+using Ethos.Domain.Common;
+using Ethos.Query.Projections;
 using Ethos.Query.Services;
 using MediatR;
 
 namespace Ethos.Application.Handlers
 {
-    public class GetAllRecurringSchedulesQueryHandler : IRequestHandler<GetAllRecurringSchedulesQuery, IEnumerable<RecurringScheduleDto>>
+    public class
+        GetAllRecurringSchedulesQueryHandler : IRequestHandler<GetAllRecurringSchedulesQuery,
+            IEnumerable<RecurringScheduleDto>>
     {
         private readonly IScheduleQueryService _scheduleQueryService;
+        private readonly IScheduleExceptionQueryService _scheduleExceptionQueryService;
 
-        public GetAllRecurringSchedulesQueryHandler(IScheduleQueryService scheduleQueryService)
+        public GetAllRecurringSchedulesQueryHandler(
+            IScheduleQueryService scheduleQueryService,
+            IScheduleExceptionQueryService scheduleExceptionQueryService)
         {
             _scheduleQueryService = scheduleQueryService;
+            _scheduleExceptionQueryService = scheduleExceptionQueryService;
         }
 
-        public async Task<IEnumerable<RecurringScheduleDto>> Handle(GetAllRecurringSchedulesQuery request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<RecurringScheduleDto>> Handle(
+            GetAllRecurringSchedulesQuery request,
+            CancellationToken cancellationToken)
         {
             var recurringSchedules = await _scheduleQueryService.GetAllRecurringSchedulesAsync();
 
-            return recurringSchedules.Select(s => new RecurringScheduleDto()
+            var result = new List<RecurringScheduleDto>();
+
+            foreach (var recurringSchedule in recurringSchedules)
             {
-                Id = s.Id,
-                Name = s.Name,
-                Description = s.Description,
-                Organizer = new RecurringScheduleDto.UserDto()
+                var nextOccurrences = await GetNextOccurrences(recurringSchedule).ToListAsync(cancellationToken);
+                result.Add(new RecurringScheduleDto()
                 {
-                    Id = s.Organizer.Id,
-                    Email = s.Organizer.Email,
-                    FullName = s.Organizer.FullName,
-                    UserName = s.Organizer.UserName,
-                },
-                DurationInMinutes = s.DurationInMinutes,
-                ParticipantsMaxNumber = s.ParticipantsMaxNumber,
-                RecurringCronExpression = s.RecurringExpression,
-                StartDate = s.StartDate,
-                EndDate = s.EndDate,
-                NextOccurrences = GetNextOccurrences(s.RecurringExpression, s.EndDate),
-            });
+                    Id = recurringSchedule.Id,
+                    Name = recurringSchedule.Name,
+                    Description = recurringSchedule.Description,
+                    Organizer = new RecurringScheduleDto.UserDto()
+                    {
+                        Id = recurringSchedule.Organizer.Id,
+                        Email = recurringSchedule.Organizer.Email,
+                        FullName = recurringSchedule.Organizer.FullName,
+                        UserName = recurringSchedule.Organizer.UserName,
+                    },
+                    DurationInMinutes = recurringSchedule.DurationInMinutes,
+                    ParticipantsMaxNumber = recurringSchedule.ParticipantsMaxNumber,
+                    RecurringCronExpression = recurringSchedule.RecurringExpression,
+                    StartDate = recurringSchedule.StartDate,
+                    EndDate = recurringSchedule.EndDate,
+                    NextOccurrences = nextOccurrences,
+                });
+            }
+
+            return result;
         }
 
-        private IEnumerable<DateTime> GetNextOccurrences(string recurringExpression, DateTime? endDate)
+        private async IAsyncEnumerable<DateTime> GetNextOccurrences(RecurringScheduleProjection schedule)
         {
-            var endDateOccurrence = endDate ?? DateTime.UtcNow.AddMonths(3);
-            var cronExpression = CronExpression.Parse(recurringExpression);
-            return cronExpression.GetOccurrences(DateTime.UtcNow, endDateOccurrence);
+            var now = DateTime.UtcNow;
+            var period = new Period(
+                schedule.StartDate >= now ? schedule.StartDate : now,
+                schedule.EndDate ?? DateTime.UtcNow.AddMonths(3));
+
+            var scheduleExceptions =
+                await _scheduleExceptionQueryService.GetScheduleExceptionsAsync(schedule.Id, period);
+
+            var nextExecutions = CronExpression.Parse(schedule.RecurringExpression).GetOccurrences(
+                fromUtc: period.StartDate,
+                toUtc: period.EndDate,
+                fromInclusive: true,
+                toInclusive: true);
+
+            foreach (var nextExecution in nextExecutions)
+            {
+                var startDate = nextExecution;
+                var endDate = nextExecution.Add(TimeSpan.FromMinutes(schedule.DurationInMinutes));
+
+                var hasExceptions = scheduleExceptions.Any(e => e.StartDate <= startDate && e.EndDate >= endDate);
+
+                if (!hasExceptions)
+                {
+                    yield return nextExecution;
+                }
+            }
         }
     }
 }

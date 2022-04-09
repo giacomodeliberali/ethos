b508507f7695d4a69ccdfdb6dc1e2644bb9fb841
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2022-04-09 17:47:12 +0200

    Add migration

---
+++ server/src/Ethos.EntityFrameworkCore/Migrations/20220409154408_AddTimeZone.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ethos.EntityFrameworkCore.Migrations
+{
+    public partial class AddTimeZone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TimeZone",
+                schema: "Schedules",
+                table: "Schedules",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeZone",
+                schema: "Schedules",
+                table: "Schedules");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
